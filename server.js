183db9e89228477e2aa0d28b31b3e281934bad57
rsenecal <roodysenecal@gmail.com>
2022-11-27 10:48:39 -0500

    Starting over Small

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require("express");
-const routes = require("./routes");
-const sequelize = require("./config/connection");
+const path = require('path');
+// const routes = require("./routes");
+// const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
-app.use(routes);
+// app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+// sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
-});
+// });
